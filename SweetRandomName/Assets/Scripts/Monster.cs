a53107d5933e06c97d9567eaf66f4c84d86521fc
Apollon76 <Apollon76@yandex.ru>
2016-04-10 18:38:40 +0500

    Add monster simple AI

--- SweetRandomName/Assets/Scripts/Monster.cs
+++ SweetRandomName/Assets/Scripts/Monster.cs
@@ -1,14 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class Monster : DarkObject {
     public int damage;
+    public GameObject player;
+    private Rigidbody2D body;
+    public Vector3 playerPosition;
+    public float maxSpeed;
 
 	void Start () {
         PreStart();
+        body = GetComponent<Rigidbody2D>();
 	}
 	
 	void Update () {
         ChangeWorld();
+        float xSpeed = 0;
+        if (isActive)
+        {
+            playerPosition = player.transform.position;
+            int dir = Math.Sign(player.transform.position.x - transform.position.x);
+            if (Vector3.Distance(player.transform.position, transform.position) < 1)
+                xSpeed = dir * maxSpeed;
+        }
+        else
+        {
+            xSpeed = 0;
+        }
+        body.velocity = new Vector2(xSpeed, body.velocity.y);
 	}
 }
